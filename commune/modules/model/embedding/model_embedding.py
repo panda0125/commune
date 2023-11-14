a6745724221f38a84c7d373277879920917c66e4
panda0125 <Panda202295@outlook.com>
2023-11-14 08:29:08 -0500

    added embedding module

---
+++ commune/modules/model/embedding/model_embedding.py
@@ -0,0 +1,13 @@
+import commune as c
+import os
+
+
+class ModelEmbedding(c.Module):
+    def __init__(self, config = None, **kwargs):
+        self.set_config(config, kwargs=kwargs)
+
+    def call(self, x:int = 1, y:int = 2) -> int:
+        c.print(self.config.sup)
+        c.print(self.config, 'This is the config, it is a Munch object')
+        return x + y
+    
